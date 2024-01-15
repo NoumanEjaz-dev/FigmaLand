8abeb8b6f799a0aa8ef05e30c36eea79a1debfa5
NoumanEjaz-dev <nomeejaniJj43@gmail.com>
2024-01-15 19:15:45 +0500

    changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home() {
             <SliderBox />
             <Footer />
             <Infoblock />
-            <Subscribe />
+            {/* <Subscribe /> */}
           </div>
         </div>
 
